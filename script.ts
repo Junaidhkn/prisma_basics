d6ec20bcb3379c7d4f52aae6204d5fdb51bbec3f
Junaidhkn <junaidhkniazi@gmail.com>
2023-01-26 22:23:33 +0500

    basic Crud

--- script.ts
+++ script.ts
@@ -1,16 +1,33 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 // A `main` function so that you can use async/await
 async function main() {
-  // ... you will write your Prisma Client queries here
+	// ... you will write your Prisma Client queries here
+	// //  Gell All Users
+	// const getAllUsers = await prisma.user.findMany();
+	// //  Create User
+	// const createUser = await prisma.user.create({
+	// 	data: {
+	// 		email: 'alice@prisma.io',
+	// 	},
+	// });
+	// //  Update the User
+	// 	const updateUser = await prisma.user.update({
+	// 		where: {
+	// 			email: 'alice@prisma.io',
+	// 		},
+	// 		data: {
+	// 			name: 'alice',
+	// 		},
+	// 	});
 }
 
 main()
-  .catch(e => {
-    throw e
-  })
-  .finally(async () => {
-    await prisma.$disconnect()
-  })
+	.catch((e) => {
+		throw e;
+	})
+	.finally(async () => {
+		await prisma.$disconnect();
+	});
